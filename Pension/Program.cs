02b3ad79c56c9042f4d456c2bc02b87be8df215e
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-01-11 10:11:20 +0100

    added % diff

--- Pension/Program.cs
+++ Pension/Program.cs
@@ -22,15 +22,17 @@ namespace Pension
             List<int> tax = savings.GetYearlyTaxes();
             List<int> resultNoYield = savings.GetYearlyResultNoYield();
 
-            Console.WriteLine("{0,5} {1,20} {2,15} {3,15}", "Year", "Savings w/ yield", "Tax", "Savings w/o yield");
+            Console.WriteLine("{0,5} {1,20} {2,15} {3,15} {4,15}", "Year", "Savings w/ yield", "Tax", "Savings w/o yield", "Diff in %");
             for (int i = 0; i <= resultWithYield.Count; i += 5)
             {
                 // when i = 5, it's actually year 6 which we don't want
                 int year = (i == 0 ? i : i - 1);
+                double diff = ((double)resultWithYield[year] / resultNoYield[year] - 1) * 100;
 
                 // carefully bruteforced formatting so the columns perfectly line up
-                Console.WriteLine("{0,5} {1,20} {2,15} {3,15}", 
-                    year + 1, resultWithYield[year].ToString("N0") + " kr", tax[year].ToString("N0") + " kr", resultNoYield[year].ToString("N0") + " kr");
+                Console.WriteLine("{0,5} {1,20} {2,15} {3,17} {4,15}",
+                    year + 1, resultWithYield[year].ToString("N0") + " kr", tax[year].ToString("N0") + " kr",
+                    resultNoYield[year].ToString("N0") + " kr", diff.ToString("N0") + "%");
             }
         }
     }
