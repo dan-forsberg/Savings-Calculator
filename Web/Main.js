ecae98e396a82421c88b86581a6a96a91536a435
Dan Örjestad <danorjest@gmail.com>
2021-01-14 20:11:12 +0100

    added chart.js

--- Web/Main.js
+++ Web/Main.js
@@ -1,6 +1,43 @@
 import { Calculator } from './Calculator.js';
+const canvas = document.getElementById('chart');
+if (canvas === null) {
+    throw new Error("Canvas not found!");
+}
 let calc = new Calculator(10000, 40, 250, 1.08, 3, 100, 1.25);
-let results = calc.calculateSavings();
-results.forEach(saving => {
-    console.log(`Year: ${saving.year}: ${saving.resultWithYield}`);
+const ctx = canvas.getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+        }
+    }
 });
--- Web/Main.ts
+++ Web/Main.ts
@@ -1,5 +1,9 @@
 import { Calculator, Savings } from './Calculator.js';
 
+const canvas = <HTMLCanvasElement>document.getElementById('chart');
+if (canvas === null) {
+    throw new Error("Canvas not found!");
+}
 let calc: Calculator = new Calculator(
     10000,
     40,
@@ -10,7 +14,41 @@ let calc: Calculator = new Calculator(
     1.25
 );
 
-let results: ReadonlyArray<Savings> = calc.calculateSavings();
-results.forEach(saving => {
-    console.log(`Year: ${saving.year}: ${saving.resultWithYield}`);
+const ctx = canvas.getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
