b68c3be0635fb34d06b2be619331f7925ffbf81b
Dan Örjestad <danorjest@gmail.com>
2021-01-14 21:28:49 +0100

    renamed yield -> yearlyYield

--- Web/Main.js
+++ Web/Main.js
@@ -7,11 +7,11 @@ if (canvas === null) {
 let startCapital = document.getElementById('startCapital').value;
 let monthlySavings = document.getElementById('monthlySavings').value;
 let savingsPeriod = document.getElementById('savingsPeriod').value;
-let yield = document.getElementById('yield').value;
+let yearlyYield = document.getElementById('yield').value;
 let schIncPeriod = document.getElementById('scheduledIncreasePeriod').value;
 let schInc = document.getElementById('scheduledIncrease').value;
 let govtIntRate = document.getElementById('govtIntRate').value;
 
-let calc = new Calculator(startCapital, savingsPeriod, monthlySavings, yield, schIncPeriod, schInc, govtIntRate);
+let calc = new Calculator(startCapital, savingsPeriod, monthlySavings, yearlyYield, schIncPeriod, schInc, govtIntRate);
 const ctx = canvas.getContext('2d');
 
