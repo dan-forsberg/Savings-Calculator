8f49ab068bdccd9a34221b8c863ce8317178093d
Dan Örjestad <danorjest@gmail.com>
2021-03-01 11:01:14 +0100

    Starting over, moving to Svelte

--- src/ISavings.ts
+++
@@ -1,7 +0,0 @@
-interface ISavings {
-    readonly year: number;
-    readonly yearlyProfit: number;
-    readonly resultWithProfit: number;
-    readonly resultNoProfit: number;
-    readonly resultDiff: number;
-}
