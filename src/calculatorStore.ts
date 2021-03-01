e1d3bf4bf4e1dc1a3e187912724535c62c5f6f1f
Dan Örjestad <danorjest@gmail.com>
2021-03-01 16:40:06 +0100

    bugfix

--- src/calculatorStore.ts
+++ src/calculatorStore.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
+import Calculator from "./scripts/calculator";
 
-const calculator = writable(null);
+const calculator = writable(new Calculator(0, 0, 0, 0, 0, 0));
 
 export default calculator;
