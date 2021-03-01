a38d0cff4390a82f3f9f9a9ec61e66c360f8e29c
Dan Örjestad <danorjest@gmail.com>
2021-03-01 16:48:59 +0100

    Started styling with flex-box

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,20 @@ import Chart from "./components/Chart.svelte";
 import Table from "./components/Table.svelte";
 </script>
 
-<Form />
-<Chart />
-<Table />
+<div id="container">
+	<Form />
+	<Chart />
+	<div>
+		<Table />
+	</div>
+</div>
 
 <style>
+#container {
+	width: 100%;
+	max-width: 100%;
+
+	display: flex;
+	justify-content: space-between;
+}
 </style>
