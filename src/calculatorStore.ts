3ed900a91f8df57cb1fdd577794735eff34fdcc1
Dan Örjestad <danorjest@gmail.com>
2021-03-09 14:23:00 +0100

    fix

--- src/calculatorStore.ts
+++ src/calculatorStore.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import Calculator from "./scripts/calculator";
 
-const calculator = writable(new Calculator(0, 0, 0, 0, 0, 0));
+const calculator = writable(new Calculator(0, 0, true, 0, 0, 0));
 
 export default calculator;
